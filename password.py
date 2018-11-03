76ee803b102fe9e7a7d50c6ee6f5083146947382
Ghost Rider <dianamulungi@gmail.com>
2018-11-04 02:44:17 +0300

    created a new class

--- password.py
+++ password.py
@@ -1,9 +1,1 @@
-class Password:
-
-    password_list = []
-
-    def _init_(self,username,passowrd,email):
-
-        self.username = username
-        self.password = password
-        self.email = email
+class Credentials:
