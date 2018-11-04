6a0abce430756e101bea8350595511f009c1531a
Ghost Rider <dianamulungi@gmail.com>
2018-11-04 03:07:08 +0300

    unittest imported and a subclass created

--- password.py
+++ password.py
@@ -1,1 +1,10 @@
 class Credentials:
+
+    credentials_list =[]
+
+    def __init__(self,firstname,lastname,phone_number,email,password):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.phone_number = phone_number
+        self.email = email
+        self.password = password
---
+++ password_test.py
@@ -0,0 +1,4 @@
+import unittest
+from password import Credentials
+
+class Credentials_test(unittest.TeatCase)
