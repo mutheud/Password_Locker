91f408a6753256d08cf242942bf18aa7dc63e84e
Ghost Rider <dianamulungi@gmail.com>
2018-11-04 03:25:50 +0300

    instance variable created

--- password_test.py
+++ password_test.py
@@ -1,4 +1,7 @@
 import unittest
 from password import Credentials
 
-class Credentials_test(unittest.TeatCase)
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+        self.new_credentials = Credentials("James","Muriuki","0712345678","james@ms.com","mutheu")
